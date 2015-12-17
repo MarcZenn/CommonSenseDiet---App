9af4d8be300acec545d64c6dffee9c0661c0cc25
marczenn <marcusareulius@gmail.com>
2015-12-17 15:09:43 -0700

    finished project scaffolding to include angular and gulp and SASS but still needs Jade

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('commonSenseDietApp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
