4b5cf5c563f21d9422341dbf5c4613f6bc71fcd8
marczenn <marcusareulius@gmail.com>
2016-01-20 18:14:49 -0700

    finished form front end and styling also downloaded normalize.css

--- src/app/main/submitNewFood.controller.js
+++ src/app/main/submitNewFood.controller.js
@@ -6,8 +6,25 @@
     .controller('submitNewFoodController', submitNewFoodController);
 
 
-  function submitNewFoodController($timeout, $scope, $location, $anchorScroll) {
-  	
+  function submitNewFoodController($timeout, $scope, $location, $anchorScroll, $https) {
+
+  	$scope.submitNewFood = function() {
+  		// Add user authentication later so users can't find this page without admin access
+
+  		console.log(food submitted)
+
+  		$http.post('/submitNewFood', $scope.newFoodFormData)
+  			.then(function(response) {
+
+  				// if http request is not successful...
+  				if () {
+  					console.log("error submitting form")
+  					// if everything is ok then...
+  				} else {
+  					
+  				}
+  			})
+  	}
   }
 
   }
