0492a6a52830f05bedeadc935b67f6c7f3f02885
Marc <MarcZenn@users.noreply.github.com>
2016-11-17 11:20:09 -0700

    Delete config.js

--- src/app/config.js
+++
@@ -1,3 +0,0 @@
-angular.module("envconfig.module", [])
-.constant("devEnvironment", {"api_url":"http://api.nal.usda.gov","api_key":"oSZ9f2ly7gkJo0A7fcRtd9z6AEGJaXIgPhNM6lhV"})
-.constant("prodEnvironment", {"api_url":"http://api.nal.usda.gov","api_key":"oSZ9f2ly7gkJo0A7fcRtd9z6AEGJaXIgPhNM6lhV"});
